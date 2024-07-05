0337d0e0d010fe9e0069314bdce67c9d18ed423c
Tom Sydney Kerckhove <syd@cs-syd.eu>
2024-07-05 13:22:37 +0200

    Error tests for reading env vars and multiple options

--- opt-env-conf-test/test/OptEnvConf/ErrorSpec.hs
+++ opt-env-conf-test/test/OptEnvConf/ErrorSpec.hs
@@ -12,32 +12,48 @@ import Text.Colour
 
 spec :: Spec
 spec = do
-  parseErrorSpec
+  parseArgsErrorSpec
     "empty"
     (empty :: Parser String)
     []
-  parseErrorSpec
+  parseArgsErrorSpec
     "missing-argument"
     (setting [reader str, argument, help "example argument", metavar "ARGUMENT"] :: Parser String)
     []
-  parseErrorSpec
+  parseArgsErrorSpec
     "missing-option"
     (setting [reader str, option, long "foo", help "example option", metavar "FOO"] :: Parser String)
     []
-  parseErrorSpec
+  parseArgsErrorSpec
+    "missing-options"
+    ( (,)
+        <$> setting [reader str, option, long "foo", help "example option 1", metavar "FOO"]
+        <*> setting [reader str, option, long "bar", help "example option 2", metavar "BAR"] ::
+        Parser (String, String)
+    )
+    []
+  parseArgsErrorSpec
     "read-int-argument"
     (setting [argument, reader auto, help "integer option", metavar "INT"] :: Parser Int)
     ["five"]
-  parseErrorSpec
+  parseArgsErrorSpec
     "read-int-option"
     (setting [option, reader auto, long "num", help "integer option", metavar "INT"] :: Parser Int)
     ["--num", "five"]
-  parseErrorSpec
+  parseArgsErrorSpec
+    "read-int-options"
+    ( (,)
+        <$> setting [option, reader auto, long "foo", help "integer option 1", metavar "INT"]
+        <*> setting [option, reader auto, long "bar", help "integer option 2", metavar "INT"] ::
+        Parser (Int, Int)
+    )
+    ["--foo", "n", "--bar", "m"]
+  parseArgsErrorSpec
     "some-none"
     (some $ setting [reader str, argument] :: Parser [String])
     []
 
-  parseErrorSpec
+  parseArgsErrorSpec
     "required-command"
     ( commands
         [ command "one" "first" $ pure '1',
@@ -46,7 +62,7 @@ spec = do
     )
     []
 
-  parseErrorSpec
+  parseArgsErrorSpec
     "unrecognised-command"
     ( commands
         [ command "one" "first" $ pure '1',
@@ -55,7 +71,7 @@ spec = do
     )
     ["three"]
 
-  parseErrorSpec
+  parseArgsErrorSpec
     "unfolding-tombstone-option"
     ( (,)
         <$> setting [option, short 'a', reader str]
@@ -64,7 +80,7 @@ spec = do
     )
     ["-ba", "foo", "bar"]
 
-  parseErrorSpec
+  parseArgsErrorSpec
     "unfolding-tombstone-switch"
     ( (,)
         <$> setting [switch (), short 'v']
@@ -73,13 +89,49 @@ spec = do
     )
     ["-fv", "foo"]
 
-parseErrorSpec :: (HasCallStack) => (Show a) => FilePath -> Parser a -> [String] -> Spec
-parseErrorSpec fp p args =
+  parseEnvErrorSpec
+    "missing-var"
+    (setting [reader str, env "FOO"] :: Parser String)
+    []
+  parseEnvErrorSpec
+    "missing-vars"
+    ( (,)
+        <$> setting [reader str, env "FOO"]
+        <*> setting [reader str, env "BAR"] ::
+        Parser (String, String)
+    )
+    []
+  parseEnvErrorSpec
+    "unreadable-var"
+    (setting [reader auto, env "FOO"] :: Parser Int)
+    [("FOO", "n")]
+  parseEnvErrorSpec
+    "unreadable-vars"
+    ( (,)
+        <$> setting [reader auto, env "FOO"]
+        <*> setting [reader auto, env "BAR"] ::
+        Parser (Int, Int)
+    )
+    [("FOO", "n"), ("BAR", "m")]
+
+parseArgsErrorSpec :: (HasCallStack) => (Show a) => FilePath -> Parser a -> [String] -> Spec
+parseArgsErrorSpec fp p args =
   withFrozenCallStack $
     it (unwords ["renders the", fp, "error the same as before"]) $
       let path = "test_resources/error/" <> fp <> ".txt"
        in goldenChunksFile path $ do
             errOrResult <- runParserOn p (parseArgs args) EnvMap.empty Nothing
+            case errOrResult of
+              Right a -> expectationFailure $ unlines ["Should not have been able to parse, but did and got:", show a]
+              Left errs -> pure $ renderErrors errs
+
+parseEnvErrorSpec :: (HasCallStack) => (Show a) => FilePath -> Parser a -> [(String, String)] -> Spec
+parseEnvErrorSpec fp p e =
+  withFrozenCallStack $
+    it (unwords ["renders the", fp, "error the same as before"]) $
+      let path = "test_resources/error/" <> fp <> ".txt"
+       in goldenChunksFile path $ do
+            errOrResult <- runParserOn p emptyArgs (EnvMap.parse e) Nothing
             case errOrResult of
               Right a -> expectationFailure $ unlines ["Should not have been able to parse, but did and got:", show a]
               Left errs -> pure $ renderErrors errs
