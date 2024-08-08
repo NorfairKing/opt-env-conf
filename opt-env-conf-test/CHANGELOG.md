ceaaa14d53d6d6dedc406fd533df39bac95be8dc
Tom Sydney Kerckhove <syd@cs-syd.eu>
2024-08-08 16:59:31 +0200

    parser testing helpers

--- opt-env-conf-test/CHANGELOG.md
+++ opt-env-conf-test/CHANGELOG.md
@@ -1,5 +1,18 @@
 # Changelog
 
+## [0.0.0.1] - 2024-08-08
+
+### Added
+
+* `settingsParserArgsTest`
+* `parserArgsTest`
+* `settingsParserEnvTest`
+* `parserEnvTest`
+* `settingsParserConfTest`
+* `parserConfTest`
+* `settingsParserTest`
+* `parserTest`
+
 ## [0.0.0.0] - 2024-07-08
 
 First version
--- opt-env-conf-test/src/OptEnvConf/Test.hs
+++ opt-env-conf-test/src/OptEnvConf/Test.hs
@@ -3,12 +3,27 @@
 {-# LANGUAGE TypeApplications #-}
 
 module OptEnvConf.Test
-  ( settingsLintSpec,
+  ( -- * Lint test
+    settingsLintSpec,
     parserLintSpec,
     parserLintTest,
+
+    -- * Parse tests
+    settingsParserArgsTest,
+    parserArgsTest,
+    settingsParserEnvTest,
+    parserEnvTest,
+    settingsParserConfTest,
+    parserConfTest,
+    settingsParserTest,
+    parserTest,
+
+    -- * Reference documentation
     goldenSettingsReferenceDocumentationSpec,
     goldenParserReferenceDocumentationSpec,
     pureGoldenReferenceDocumentation,
+
+    -- * Nix options
     goldenSettingsNixOptionsSpec,
     goldenParserNixOptionsSpec,
     pureGoldenNixOptions,
@@ -16,10 +31,14 @@ module OptEnvConf.Test
   )
 where
 
+import Data.Aeson as JSON
 import Data.Text (Text)
 import qualified Data.Text as T
 import GHC.Stack (HasCallStack, withFrozenCallStack)
 import OptEnvConf
+import OptEnvConf.Args
+import OptEnvConf.EnvMap as EnvMap
+import OptEnvConf.Error
 import OptEnvConf.Lint
 import Test.Syd
 import Text.Colour
@@ -39,6 +58,34 @@ parserLintTest parser =
     Nothing -> pure ()
     Just errs ->
       expectationFailure $ T.unpack $ renderChunksText With24BitColours $ renderLintErrors errs
+
+settingsParserArgsTest :: (Show a, Eq a, HasParser a) => [String] -> a -> IO ()
+settingsParserArgsTest = parserArgsTest settingsParser
+
+parserArgsTest :: (Show a, Eq a) => Parser a -> [String] -> a -> IO ()
+parserArgsTest parser args = parserTest parser args [] Nothing
+
+settingsParserEnvTest :: (Show a, Eq a, HasParser a) => [(String, String)] -> a -> IO ()
+settingsParserEnvTest = parserEnvTest settingsParser
+
+parserEnvTest :: (Show a, Eq a) => Parser a -> [(String, String)] -> a -> IO ()
+parserEnvTest parser envVars = parserTest parser [] envVars Nothing
+
+settingsParserConfTest :: (Show a, Eq a, HasParser a) => JSON.Object -> a -> IO ()
+settingsParserConfTest = parserConfTest settingsParser
+
+parserConfTest :: (Show a, Eq a) => Parser a -> JSON.Object -> a -> IO ()
+parserConfTest parser obj = parserTest parser [] [] (Just obj)
+
+settingsParserTest :: (Show a, Eq a, HasParser a) => [String] -> [(String, String)] -> Maybe JSON.Object -> a -> IO ()
+settingsParserTest = parserTest settingsParser
+
+parserTest :: (Show a, Eq a) => Parser a -> [String] -> [(String, String)] -> Maybe JSON.Object -> a -> IO ()
+parserTest parser args envVars mObject expected = do
+  errOrActual <- runParserOn Nothing parser (parseArgs args) (EnvMap.parse envVars) mObject
+  case errOrActual of
+    Left errs -> expectationFailure $ T.unpack $ renderChunksText With24BitColours $ renderErrors errs
+    Right actual -> actual `shouldBe` expected
 
 goldenSettingsReferenceDocumentationSpec :: forall a. (HasCallStack) => (HasParser a) => FilePath -> String -> Spec
 goldenSettingsReferenceDocumentationSpec path progname = withFrozenCallStack $ goldenParserReferenceDocumentationSpec (settingsParser @a) path progname
