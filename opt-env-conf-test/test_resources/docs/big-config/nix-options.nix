dcc6dbd8af9460e267e10704301c011a95c0de84
Tom Sydney Kerckhove <syd@cs-syd.eu>
2024-09-13 18:46:23 +0200

    update inputs

--- opt-env-conf-test/test_resources/docs/big-config/nix-options.nix
+++ opt-env-conf-test/test_resources/docs/big-config/nix-options.nix
@@ -6,8 +6,7 @@
     type = lib.types.nullOr (lib.types.submodule {
       options = {
         map = lib.mkOption {
-          default = null;
-          type = lib.types.nullOr (lib.types.attrsOf (lib.types.attrsOf lib.types.int));
+          type = lib.types.attrsOf (lib.types.attrsOf lib.types.int);
         };
         sub = lib.mkOption {
           default = null;
