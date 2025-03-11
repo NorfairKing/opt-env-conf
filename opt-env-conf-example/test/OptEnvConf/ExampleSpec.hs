ab673412f6614b19b23a61f0d33679e06da8d8c3
Tom Sydney Kerckhove <syd@cs-syd.eu>
2025-03-11 10:13:25 +0100

    fix the build

--- opt-env-conf-example/test/OptEnvConf/ExampleSpec.hs
+++ opt-env-conf-example/test/OptEnvConf/ExampleSpec.hs
@@ -4,6 +4,7 @@
 
 module OptEnvConf.ExampleSpec where
 
+import Data.Maybe
 import OptEnvConf.Completion
 import OptEnvConf.Example
 import OptEnvConf.Test
@@ -48,36 +49,46 @@ spec = do
   describe "Completion" $ do
     let p = settingsParser @Instructions
     it "auto-completes the commands and top-level settings" $
-      pureCompletionQuery p 0 []
-        `shouldSuggest` [ Completion "create" $ Just "Create",
-                          Completion "read" $ Just "Read",
-                          Completion "update" $ Just "Update",
-                          Completion "delete" $ Just "Delete",
-                          Completion "--log-level" $ Just "minimal severity of log messages",
-                          Completion "--base" $ Just "base directory for items",
-                          Completion "--payment-public-key" $ Just "Public key",
-                          Completion "--payment-secret-key" $ Just "Secret key",
-                          Completion "--payment-currency" $ Just "Currency",
-                          Completion "--config-file" $ Just "Path to the configuration file"
-                        ]
+      shouldSuggest
+        0
+        []
+        [ Completion "create" $ Just "Create",
+          Completion "read" $ Just "Read",
+          Completion "update" $ Just "Update",
+          Completion "delete" $ Just "Delete",
+          Completion "--log-level" $ Just "minimal severity of log messages",
+          Completion "--base" $ Just "base directory for items",
+          Completion "--payment-public-key" $ Just "Public key",
+          Completion "--payment-secret-key" $ Just "Secret key",
+          Completion "--payment-currency" $ Just "Currency",
+          Completion "--config-file" $ Just "Path to the configuration file"
+        ]
     it "auto-completes the create file option dashed" $
-      pureCompletionQuery p 1 ["create", "-"]
-        `shouldSuggest` [ Completion "--file" $ Just "file to create the item in",
-                          Completion "--log-level" $ Just "minimal severity of log messages",
-                          Completion "--base" $ Just "base directory for items",
-                          Completion "--payment-public-key" $ Just "Public key",
-                          Completion "--payment-secret-key" $ Just "Secret key",
-                          Completion "--payment-currency" $ Just "Currency",
-                          Completion "--config-file" $ Just "Path to the configuration file"
-                        ]
+      shouldSuggest
+        1
+        ["create", "-"]
+        [ Completion "--file" $ Just "file to create the item in",
+          Completion "--log-level" $ Just "minimal severity of log messages",
+          Completion "--base" $ Just "base directory for items",
+          Completion "--payment-public-key" $ Just "Public key",
+          Completion "--payment-secret-key" $ Just "Secret key",
+          Completion "--payment-currency" $ Just "Currency",
+          Completion "--config-file" $ Just "Path to the configuration file"
+        ]
     it "auto-completes the create file option files" $
       case pureCompletionQuery p 2 ["create", "--file"] of
         [] -> expectationFailure "Expected only a file completion, got none"
         [Completion (SuggestionCompleter (Completer _)) (Just "file to create the item in")] -> pure () -- We assume that it's the file completer so we don't have to run this in a temp dir.
         cs -> expectationFailure $ "Expected only a file completion, got more: " <> show (length cs)
 
-shouldSuggest :: [Completion Suggestion] -> [Completion Suggestion] -> IO ()
-shouldSuggest cs1 cs2 = do
-  s1s <- evalCompletions cs1
-  s2s <- evalCompletions cs2
-  s1s `shouldBe` s2s
+shouldSuggest :: Int -> [String] -> [Completion String] -> IO ()
+shouldSuggest ix ws expected = do
+  let arg = fromMaybe "" $ listToMaybe $ drop ix ws
+  let completions = pureCompletionQuery (settingsParser @Instructions) ix ws
+  evaluatedCompletions <- evalCompletions arg completions
+  evaluatedCompletions `shouldBe` expected
+
+shouldSuggestDesc :: Int -> [String] -> [String] -> IO ()
+shouldSuggestDesc ix ws descriptions = do
+  let completions = pureCompletionQuery (settingsParser @Instructions) ix ws
+  map completionDescription completions `shouldBe` map Just descriptions
