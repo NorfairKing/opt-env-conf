4f701223a422f35a74e686b376c1e7a9017b577c
Tom Sydney Kerckhove <syd@cs-syd.eu>
2025-10-28 15:08:28 +0100

    simpl impl

--- opt-env-conf/src/OptEnvConf/Check.hs
+++ opt-env-conf/src/OptEnvConf/Check.hs
@@ -5,6 +5,9 @@ module OptEnvConf.Check
   )
 where
 
+import qualified Data.Aeson.Types as JSON
+import Data.List.NonEmpty (NonEmpty)
+import qualified Data.List.NonEmpty as NE
 import OptEnvConf.Args as Args
 import OptEnvConf.EnvMap (EnvMap (..))
 import OptEnvConf.Error
@@ -18,12 +21,25 @@ import Text.Colour
 runSettingsCheck :: Parser a -> Args -> EnvMap -> IO void
 runSettingsCheck p args envVars = do
   stderrTc <- getTerminalCapabilitiesFromHandle stderr
-  errOrSets <- runParserOn (Just stderrTc) p args envVars Nothing
+  errOrSets <- runSettingsCheckOn (Just stderrTc) p args envVars Nothing
   case errOrSets of
-    Left errs -> do
+    Just errs -> do
       hPutChunksLocaleWith stderrTc stderr $ renderErrors errs
       exitFailure
-    Right _ -> do
+    Nothing -> do
       tc <- getTerminalCapabilitiesFromHandle stdout
-      hPutChunksLocaleWith tc stdout ["Settings parsed successfully."]
+      hPutChunksLocaleWith tc stdout ["Settings check successful."]
       exitSuccess
+
+runSettingsCheckOn ::
+  Maybe TerminalCapabilities ->
+  Parser a ->
+  Args ->
+  EnvMap ->
+  Maybe JSON.Object ->
+  IO (Maybe (NonEmpty ParseError))
+runSettingsCheckOn mDebugMode parser args envVars mConfig = do
+  errsOrResult <- runParserOn mDebugMode parser args envVars mConfig
+  case errsOrResult of
+    Left errs -> return (Just errs)
+    Right _settings -> return Nothing
