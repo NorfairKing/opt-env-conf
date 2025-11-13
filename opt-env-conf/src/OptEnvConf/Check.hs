9e51980c4df3f7fb37573825c94cf63688ec182e
Tom Sydney Kerckhove <syd@cs-syd.eu>
2025-11-13 11:59:36 +0100

    Failing test

--- opt-env-conf/src/OptEnvConf/Check.hs
+++ opt-env-conf/src/OptEnvConf/Check.hs
@@ -8,15 +8,19 @@ module OptEnvConf.Check
   )
 where
 
+import Control.Monad.Reader hiding (Reader, reader, runReader)
+import Control.Monad.State
 import qualified Data.Aeson.Types as JSON
 import Data.List.NonEmpty (NonEmpty)
 import qualified Data.List.NonEmpty as NE
 import OptEnvConf.Args as Args
 import OptEnvConf.EnvMap (EnvMap (..))
 import OptEnvConf.Error
+import OptEnvConf.NonDet
 import OptEnvConf.Parser
 import OptEnvConf.Run
 import OptEnvConf.Terminal (getTerminalCapabilitiesFromHandle)
+import OptEnvConf.Validation
 import System.Exit
 import System.IO (stderr, stdout)
 import Text.Colour
@@ -42,14 +46,40 @@ runSettingsCheckOn ::
   Maybe JSON.Object ->
   IO (Maybe (NonEmpty ParseError))
 runSettingsCheckOn mDebugMode parser args envVars mConfig = do
-  errsOrResult <- runParserOn mDebugMode parser args envVars mConfig
-  case errsOrResult of
-    Left errs -> return (Just errs)
-    Right _settings -> return Nothing
+  let ppState = mkPPState args
+  let ppEnv = mkPPEnv envVars mConfig mDebugMode
+  let go' = do
+        result <- go parser
+        leftoverArgs <- liftPP $ gets ppStateArgs
+        case recogniseLeftovers leftoverArgs of
+          Nothing -> pure result
+          Just leftovers -> liftPP $ ppError Nothing $ ParseErrorUnrecognised leftovers
+  mTup <- runPPLazy (unChecker go') ppState ppEnv
+  case mTup of
+    Nothing -> error "TODO figure out when this list can be empty"
+    Just ((errOrRes, _), nexts) -> case errOrRes of
+      Success _ -> pure Nothing
+      Failure firstErrors ->
+        let goNexts ns = do
+              -- TODO: Consider keeping around all errors?
+              mNext <- runNonDetTLazy ns
+              case mNext of
+                Nothing ->
+                  pure $
+                    Just $
+                      let f = case mDebugMode of
+                            Nothing -> eraseErrorSrcLocs
+                            Just _ -> id
+                       in f firstErrors
+                Just ((eOR, _), ns') -> case eOR of
+                  Success _ -> pure Nothing
+                  Failure _ -> goNexts ns'
+         in goNexts nexts
   where
     go :: Parser a -> Checker a
     go = \case
       ParserPure a -> pure a
+      ParserSetting mLoc set -> liftPP $ ppSetting mLoc set
 
 newtype Checker a = Checker {unChecker :: PP a}
   deriving
