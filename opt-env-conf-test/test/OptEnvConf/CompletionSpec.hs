9b66c41115f91b31351f52bee3456ab8ca3aca41
Tom Sydney Kerckhove <syd@cs-syd.eu>
2026-03-16 15:40:57 +0100

    Add comprehensive completion tests for all Parser constructors
    Cover: many, some, allOrNothing, check/mapIO, hidden settings,
    enableDisableSwitch, choice, withDefault, nested commands,
    commands via alternative, double dash, mixed args/options, select,
    empty, pure, env-only settings, and multiple arguments.
    
    Four tests for many/some fail: the ParserMany and ParserSome
    handlers duplicate completions because they call go twice without
    deduplicating.

--- opt-env-conf-test/test/OptEnvConf/CompletionSpec.hs
+++ opt-env-conf-test/test/OptEnvConf/CompletionSpec.hs
@@ -4,6 +4,7 @@
 module OptEnvConf.CompletionSpec (spec) where
 
 import Control.Applicative
+import qualified Data.List.NonEmpty as NE
 import OptEnvConf.Completer
 import OptEnvConf.Completion
 import OptEnvConf.Parser
@@ -394,3 +395,358 @@ spec = do
           1
           ["--verbose"]
           [Completion "foo" (Just "1"), Completion "bar" (Just "2"), "--example"]
+
+    describe "many" $ do
+      it "can complete repeated switches" $
+        parserCompletionTest
+          (length <$> many (setting [switch (), long "verbose", short 'v']))
+          0
+          []
+          ["--verbose"]
+
+      it "can still complete after consuming one" $
+        parserCompletionTest
+          (length <$> many (setting [switch (), long "verbose", short 'v']))
+          1
+          ["--verbose"]
+          ["--verbose"]
+
+      it "can still complete after consuming two" $
+        parserCompletionTest
+          (length <$> many (setting [switch (), long "verbose", short 'v']))
+          2
+          ["-v", "-v"]
+          ["--verbose"]
+
+      it "can complete repeated options" $
+        parserCompletionTest
+          ( many
+              ( setting
+                  [ option,
+                    reader (str :: Reader String),
+                    long "include",
+                    completer $ listCompleter ["foo", "bar"]
+                  ]
+              )
+          )
+          1
+          ["--include"]
+          ["foo", "bar"]
+
+      it "can complete repeated options after one has already been parsed" $
+        parserCompletionTest
+          ( many
+              ( setting
+                  [ option,
+                    reader (str :: Reader String),
+                    long "include",
+                    completer $ listCompleter ["foo", "bar"]
+                  ]
+              )
+          )
+          3
+          ["--include", "foo", "--include"]
+          ["foo", "bar"]
+
+    describe "some" $ do
+      it "can complete at least one switch" $
+        parserCompletionTest
+          (NE.length <$> someNonEmpty (setting [switch (), long "verbose", short 'v']))
+          0
+          []
+          ["--verbose"]
+
+      it "can still complete after consuming one" $
+        parserCompletionTest
+          (NE.length <$> someNonEmpty (setting [switch (), long "verbose", short 'v']))
+          1
+          ["--verbose"]
+          ["--verbose"]
+
+    describe "allOrNothing" $ do
+      it "completes normally inside allOrNothing" $
+        parserCompletionTest
+          ( allOrNothing $
+              (,)
+                <$> setting [option, reader (str :: Reader String), long "host"]
+                <*> setting [option, reader (str :: Reader String), long "port"]
+          )
+          0
+          []
+          ["--host", "--port"]
+
+      it "still suggests the remaining option after one is consumed" $
+        parserCompletionTest
+          ( allOrNothing $
+              (,)
+                <$> setting [option, reader (str :: Reader String), long "host"]
+                <*> setting [option, reader (str :: Reader String), long "port"]
+          )
+          2
+          ["--host", "localhost"]
+          ["--port"]
+
+    describe "check" $ do
+      it "completes through a checkMapEither" $
+        parserCompletionTest
+          ( checkMapEither
+              (\s -> if null s then Left "empty" else Right s)
+              (setting [argument, reader (str :: Reader String), completer $ listCompleter ["hello"]])
+          )
+          0
+          []
+          ["hello"]
+
+      it "completes through a mapIO" $
+        parserCompletionTest
+          ( mapIO pure $
+              setting [switch (), long "example"]
+          )
+          0
+          []
+          ["--example"]
+
+    describe "hidden settings" $ do
+      it "does not suggest hidden switches" $
+        parserCompletionTest
+          ( (,)
+              <$> setting [switch (), long "visible"]
+              <*> setting [switch (), long "secret", hidden]
+          )
+          0
+          []
+          ["--visible"]
+
+      it "does not suggest hidden options" $
+        parserCompletionTest
+          ( (,)
+              <$> setting [option, reader (str :: Reader String), long "visible"]
+              <*> setting [option, reader (str :: Reader String), long "secret", hidden]
+          )
+          0
+          []
+          ["--visible"]
+
+    describe "settings with only env or conf" $ do
+      it "does not error on env-only settings combined with arg settings" $
+        parserCompletionTest
+          ( (,)
+              <$> setting [switch (), long "verbose"]
+              <*> setting [reader (str :: Reader String), env "MY_VAR"]
+          )
+          0
+          []
+          ["--verbose"]
+
+    describe "enableDisableSwitch" $ do
+      -- enableDisableSwitch uses a visible "dummy" with the
+      -- (enable|disable)- prefix for documentation, while the actual
+      -- --enable-feature and --disable-feature switches are hidden.
+      it "completes the documented dummy flag" $
+        parserCompletionTest
+          ( enableDisableSwitch
+              [ help "enable feature",
+                name "feature",
+                value False
+              ]
+          )
+          0
+          []
+          [Completion "--(enable|disable)-feature" (Just "enable feature")]
+
+      it "filters the documented flag by prefix" $
+        parserCompletionTest
+          ( enableDisableSwitch
+              [ help "enable feature",
+                name "feature",
+                value False
+              ]
+          )
+          0
+          ["--(e"]
+          [Completion "--(enable|disable)-feature" (Just "enable feature")]
+
+    describe "choice" $ do
+      it "completes across all choices" $
+        parserCompletionTest
+          ( choice
+              [ setting [switch (), long "alpha"],
+                setting [switch (), long "beta"],
+                setting [switch (), long "gamma"]
+              ]
+          )
+          0
+          []
+          ["--alpha", "--beta", "--gamma"]
+
+    describe "withDefault" $ do
+      it "completes the underlying parser" $
+        parserCompletionTest
+          (withDefault "default" $ setting [option, reader (str :: Reader String), long "name", completer $ listCompleter ["alice", "bob"]])
+          1
+          ["--name"]
+          ["alice", "bob"]
+
+      it "still suggests the option when it has a default" $
+        parserCompletionTest
+          (withDefault "default" $ setting [option, reader (str :: Reader String), long "name"])
+          0
+          []
+          ["--name"]
+
+    describe "nested commands" $ do
+      let p =
+            commands
+              [ command "top" "top-level" $
+                  commands
+                    [ command "sub-a" "sub a" $ pure (),
+                      command "sub-b" "sub b" $ pure ()
+                    ],
+                command "other" "other" $ pure ()
+              ]
+
+      it "completes top-level commands" $
+        parserCompletionTest
+          p
+          0
+          []
+          [Completion "top" (Just "top-level"), Completion "other" (Just "other")]
+
+      it "completes sub-commands after selecting a top-level command" $
+        parserCompletionTest
+          p
+          1
+          ["top"]
+          [Completion "sub-a" (Just "sub a"), Completion "sub-b" (Just "sub b")]
+
+      it "filters sub-commands by prefix" $
+        parserCompletionTest
+          p
+          1
+          ["top", "sub-a"]
+          [Completion "sub-a" (Just "sub a")]
+
+      it "completes nested default commands" $
+        parserCompletionTest
+          ( commands
+              [ command "top" "top-level" $
+                  commands
+                    [ command "sub-a" "sub a" $ setting [switch (), long "flag"],
+                      command "sub-b" "sub b" $ pure (),
+                      defaultCommand "sub-a"
+                    ],
+                command "other" "other" $ pure ()
+              ]
+          )
+          1
+          ["top"]
+          [Completion "sub-a" (Just "sub a"), Completion "sub-b" (Just "sub b"), "--flag"]
+
+    describe "commands merged via alternative" $ do
+      it "completes commands from both sides of an alternative" $
+        parserCompletionTest
+          ( commands [command "foo" "1" $ pure ()]
+              <|> commands [command "bar" "2" $ pure ()]
+          )
+          0
+          []
+          [Completion "foo" (Just "1"), Completion "bar" (Just "2")]
+
+    describe "arguments after double dash" $ do
+      -- A bare -- is consumed as the argument value "--" when it's
+      -- the only argument remaining, so completion sees nothing left.
+      it "consumes -- as the argument value" $
+        parserCompletionTest
+          (setting [argument, reader (str :: Reader String), completer $ listCompleter ["file1", "file2"]])
+          1
+          ["--"]
+          []
+
+      -- After --, the completion should suggest positional arguments
+      -- rather than switches. Currently it does not: the switch is
+      -- still suggested because the completion engine does not
+      -- account for -- separating options from arguments.
+      it "suggests the switch even after -- (known bug)" $
+        parserCompletionTest
+          ( (,)
+              <$> setting [switch (), long "verbose"]
+              <*> setting [argument, reader (str :: Reader String), completer $ listCompleter ["file1", "file2"]]
+          )
+          2
+          ["--", "fi"]
+          ["--verbose"]
+
+    describe "mixed arguments and options" $ do
+      it "can complete an option after a positional argument" $
+        parserCompletionTest
+          ( (,)
+              <$> setting [argument, reader (str :: Reader String), completer $ listCompleter ["input"]]
+              <*> setting [switch (), long "verbose"]
+          )
+          1
+          ["input"]
+          ["--verbose"]
+
+      it "can complete a positional after an option" $
+        parserCompletionDescriptionTest
+          ( (,)
+              <$> setting [switch (), long "verbose"]
+              <*> setting [argument, reader (str :: Reader String), help "file", completer $ listCompleter ["output"]]
+          )
+          1
+          ["--verbose"]
+          ["file"]
+
+    describe "multiple arguments" $ do
+      -- When no args have been typed, both argument completers fire
+      -- because the completion engine doesn't know which one will be
+      -- consumed first. This is the documented TODO in Completion.hs.
+      it "completes both arguments when none have been typed" $
+        parserCompletionTest
+          ( (,)
+              <$> setting [argument, reader (str :: Reader String), completer $ listCompleter ["src"]]
+              <*> setting [argument, reader (str :: Reader String), completer $ listCompleter ["dst"]]
+          )
+          0
+          []
+          ["src", "dst"]
+
+      it "completes the second argument after the first" $
+        parserCompletionTest
+          ( (,)
+              <$> setting [argument, reader (str :: Reader String), completer $ listCompleter ["src"]]
+              <*> setting [argument, reader (str :: Reader String), completer $ listCompleter ["dst"]]
+          )
+          1
+          ["something"]
+          ["dst"]
+
+    describe "empty parser" $ do
+      it "produces no completions for empty" $
+        parserCompletionTest
+          (empty :: Parser ())
+          0
+          []
+          []
+
+    describe "pure parser" $ do
+      it "produces no completions for pure" $
+        parserCompletionTest
+          (pure () :: Parser ())
+          0
+          []
+          []
+
+    describe "select" $ do
+      -- Select uses andCompletions: both branches contribute because
+      -- the completion engine cannot know at completion time whether
+      -- the first parser will return Left or Right.
+      it "completes through a select" $
+        parserCompletionTest
+          ( select
+              (Left <$> setting [argument, reader (str :: Reader String), completer $ listCompleter ["hello"]])
+              (const <$> setting [argument, reader (str :: Reader String), completer $ listCompleter ["world"]])
+          )
+          0
+          []
+          ["hello", "world"]
